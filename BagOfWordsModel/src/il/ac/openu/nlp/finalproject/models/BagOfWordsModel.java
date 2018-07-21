f8588e96c05fbd8ea48a4d53190c0ee0e5d376fd
Asi Lev <asi.lev@gmail.com>
2018-07-21 20:52:49 +0300

    Different Data Models for logistic regression x

--- BagOfWordsModel/src/il/ac/openu/nlp/finalproject/models/BagOfWordsModel.java
+++ BagOfWordsModel/src/il/ac/openu/nlp/finalproject/models/BagOfWordsModel.java
@@ -15,21 +15,32 @@ public class BagOfWordsModel {
 		dataReader = new DataReader(args[0], "UTF8");
 		Map<String, BagOfWords> authorBagOfWords = dataReader.setAuthorBagOfWords();
 		double[][] authorWordsArray;
+//		List<ArrayList<Double>> authorWordsArray;
 		authorWordsArray = new double[authorBagOfWords.size()][];
+//		authorWordsArray = new ArrayList<>();
+//		for (int i=0;i<authorBagOfWords.size();++i) {
+//			authorWordsArray.add(new ArrayList<Double>());
+//		}
 		for (Map.Entry<String, BagOfWords> e : authorBagOfWords.entrySet()) {
 			BagOfWords bagOfWords = e.getValue();
 			if (!authorIndex.containsKey(e.getKey())) {
 				authorIndex.put(e.getKey(), newAuthorIndex++);
 			}
 			System.out.println(newAuthorIndex);
+			System.out.println("Allocating "+(newWordIndex+e.getValue().size()));
 			authorWordsArray[authorIndex.get(e.getKey())] = new double[newWordIndex+e.getValue().size()];
-			
+//			ArrayList<Double> newDoubleList = new ArrayList<Double>(newWordIndex+e.getValue().size());
+//			authorWordsArray.set(authorIndex.get(e.getKey()), new ArrayList<Double>());
+//			for (int i=0; i<newWordIndex+e.getValue().size();++i) {
+//				authorWordsArray.get(authorIndex.get(e.getKey())).add(new Double(0.0));
+//			}
 			for (String word : bagOfWords.getWords()) {
 				if (!wordsIndex.containsKey(word)) {
 					wordsIndex.put(word, newWordIndex++);
 				}
-				System.out.println("word "+newWordIndex);
+				System.out.println("word "+wordsIndex.get(word));
 				authorWordsArray[authorIndex.get(e.getKey())][wordsIndex.get(word)] = bagOfWords.getNumOfOccurances(word);
+//				authorWordsArray.get(authorIndex.get(e.getKey())).set(wordsIndex.get(word), new Double(bagOfWords.getNumOfOccurances(word)));
 			}
 		}		
 	}
--- LogisticRegressionModel/src/il/ac/openu/nlp/finalproject/models/LogisticRegressionModel.java
+++ LogisticRegressionModel/src/il/ac/openu/nlp/finalproject/models/LogisticRegressionModel.java
@@ -2,22 +2,38 @@ package il.ac.openu.nlp.finalproject.models;
 
 public class LogisticRegressionModel {
 	private static double x[][];
+//	private static Map<String, Map<String, Integer>> x;
+//	private static List<List<Double>> x;
 	private static int y[];
 	private static double ALPHA = 0.01;
 	private static int numOfIterations = 5000;
 	private static double j(double[] theta) throws Exception {
 		double cost = 0;
-		int m = x.length;
-		for (int i=0;i<m;++i) {
+		int numOfTrainingDataSet = x.length;
+//		int numOfTrainingDataSet = x.size();
+//		int i=0;
+//		for (Map.Entry<String, Map<String, Integer>> dataEntry : x.entrySet()) {
+//			cost += cost(h(theta, dataEntry),y[i]);
+//			i++;
+//		}
+		for (int i=0;i<numOfTrainingDataSet;++i) {
 			cost += cost(h(theta, x[i]),y[i]);
+//			cost += cost(h(theta, x.get(i)),y[i]);
 		}
-		return cost/m;
+		return cost/numOfTrainingDataSet;
 	}
 	
 	public static double h(double[] theta, double[] x) {
+//	public static double h(double[] theta, Map.Entry<String, Map<String, Integer>> dataEntry) {
+//	public static double h(double[] theta, List<Double> x) {
 		double thetaX = theta[0];
+//		int i=0;
 		for (int i=0;i<x.length;++i) {
+//		for (Map.Entry<String, Integer> feature : dataEntry.getValue().entrySet()) {
 			thetaX += theta[i+1]*x[i];
+//			thetaX += theta[i+1]*feature.getValue();
+//			thetaX += theta[i+1]*x.get(i);
+//			i++;
 		}
 		return g(thetaX);
 	}
@@ -51,8 +67,12 @@ public class LogisticRegressionModel {
 		int m=x.length;
 		double returnValue = 0;
 		for (int i=0;i<m;++i) {
+//		for (Map.Entry<String, Map<String, Integer>> dataEntry : x.entrySet()) {
 			double xij = (j==0)?1:x[i][j-1];
+//			double xij = (j==0)?1:x.get(dataEntry.getKey()).get(j)
+//			double xij = (j==0)?1:x.get(i).get(j-1);
 			returnValue += (h(theta, x[i])-y[i])*xij;
+//			returnValue += (h(theta, x.get(i))-y[i])*xij;
 		}
 		return returnValue;
 	}
@@ -68,22 +88,51 @@ public class LogisticRegressionModel {
 	
 	public static void main(String[] args) throws Exception {
 		x = new double[4][5];
+//		x = new ArrayList<>(4);
 		y = new int[4];
+//		x.add(new ArrayList<>(5));
+//		List<Double> x0 = x.get(0);
+//		x0.add(7.0);
+//		x0.add(8.0);
+//		x0.add(5.0);
+//		x0.add(9.0);
+//		x0.add(1.0);
 		x[0][0] = 7;
 		x[0][1] = 8;
 		x[0][2] = 5;
 		x[0][3] = 9;
 		x[0][4] = 1;
+//		x.add(new ArrayList<>(5));
+//		List<Double> x1 = x.get(1);
+//		x1.add(0.1);
+//		x1.add(1.0);
+//		x1.add(2.0);
+//		x1.add(1.0);
+//		x1.add(3.0);
 		x[1][0] = 0.1;
 		x[1][1] = 1;
 		x[1][2] = 2;
 		x[1][3] = 1;
 		x[1][4] = 3;
+//		x.add(new ArrayList<>(5));
+//		List<Double> x2 = x.get(2);
+//		x2.add(9.0);
+//		x2.add(3.0);
+//		x2.add(4.0);
+//		x2.add(5.0);
+//		x2.add(1.0);
 		x[2][0] = 9;
 		x[2][1] = 3;
 		x[2][2] = 4;
 		x[2][3] = 5;
 		x[2][4] = 1;
+//		x.add(new ArrayList<>(5));
+//		List<Double> x3 = x.get(3);
+//		x3.add(0.1);
+//		x3.add(0.1);
+//		x3.add(3.0);
+//		x3.add(9.0);
+//		x3.add(2.0);
 		x[3][0] = 0.1;
 		x[3][1] = 0.1;
 		x[3][2] = 3;
@@ -121,6 +170,12 @@ public class LogisticRegressionModel {
 
 		
 		double[] evalX = new double[5];
+//		List<Double> evalX = new ArrayList<>(5);
+//		evalX.add(1.0);
+//		evalX.add(1.0);
+//		evalX.add(0.0);
+//		evalX.add(1.0);
+//		evalX.add(0.0);
 		evalX[0]=1;
 		evalX[1]=1;
 		evalX[2]=0;
