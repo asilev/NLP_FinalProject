a7fbbd43d2d5bc6a543a69faba63c182501131c2
Asi Lev <asi.lev@gmail.com>
2018-08-01 22:51:30 +0300

    refactoring

--- LogisticRegressionModel/src/il/ac/openu/nlp/finalproject/models/LogisticRegressionModel.java
+++ LogisticRegressionModel/src/il/ac/openu/nlp/finalproject/models/LogisticRegressionModel.java
@@ -9,9 +9,10 @@ import org.apache.commons.lang3.NotImplementedException;
 public class LogisticRegressionModel {
 	private TrainingDataWriter trainingDataWriter; //TODO
 	private TrainingDataReader trainingDataReader; //TODO
-	private static TrainingDataRecord<String>[] data;
+	private static TrainingDataRecord<String>[] trainingData;
 	private static double ALPHA = 0.01;
 	private static int numOfIterations = 5000;
+	private static final String ZERO_INDEX = "Zero"; 
 	
 	public LogisticRegressionModel(String trainingFileName) throws IOException {
 		throw new NotImplementedException("File Constructor");
@@ -20,26 +21,26 @@ public class LogisticRegressionModel {
 	}
 	
 	public LogisticRegressionModel(TrainingDataRecord<String>[] data) {
-		LogisticRegressionModel.data = data;
+		LogisticRegressionModel.trainingData = data;
 	}
 
-	public static double h(Map<String, Double> theta, FeatureVector<String> x) {
-		double thetaX = theta.get("Zero");
-		for (String key : theta.keySet()) {
-			thetaX += theta.get(key)*x.getFeature(key);
+	public static double h(Map<String, Double> parameters, FeatureVector<String> featureVector) {
+		double parametersXfeatures = parameters.get(ZERO_INDEX); // Initial
+		for (String featureKey : parameters.keySet()) {
+			parametersXfeatures += parameters.get(featureKey)*featureVector.getFeature(featureKey);
 		}
-		return g(thetaX);
+		return g(parametersXfeatures);
 	}
 
-	private static double g(double z) {
+	private static double g(double z) { // Sigmoid Function
 		return 1.0/(1+Math.pow(Math.E, -1*z));
 	}
 	
-	private static double j(Map<String, Double> theta, String target) throws Exception {
+	private static double j(Map<String, Double> parameters, String targetClass) throws Exception {
 		double cost = 0;
-		int numOfTrainingDataSet = data.length;
+		int numOfTrainingDataSet = trainingData.length;
 		for (int i=0;i<numOfTrainingDataSet;++i) {
-			cost += cost(h(theta, data[i].getTrainingFeaturesVector()),data[i].getTrainingDataRecordClass().equals(target)?1:0);
+			cost += cost(h(parameters, trainingData[i].getTrainingFeaturesVector()),trainingData[i].getTrainingDataRecordClass().equals(targetClass)?1:0);
 		}
 		return cost/numOfTrainingDataSet;
 	}
@@ -56,51 +57,51 @@ public class LogisticRegressionModel {
 		}
 	}
 	
-	private static Map<String, Double> minJ(Map<String, Double> theta, String klass) {
-		Map<String, Double> returnTheta = new HashMap<>();
-		int m = data.length;
-		for (String key : theta.keySet()) {
-			double value = theta.get(key) - (ALPHA/m)*sumDerivations(theta, key, klass);
-			returnTheta.put(key, value);
+	private static Map<String, Double> minJ(Map<String, Double> parameters, String targetClass) {
+		Map<String, Double> returnParameters = new HashMap<>();
+		int m = trainingData.length;
+		for (String featureKey : parameters.keySet()) {
+			double returnParameterValue = parameters.get(featureKey) - (ALPHA/m)*sumDerivations(parameters, featureKey, targetClass);
+			returnParameters.put(featureKey, returnParameterValue);
 		}
-		return returnTheta;
+		return returnParameters;
 	}
 	
-	private static double sumDerivations(Map<String, Double>theta, String j, String klass) {
-		int m = data.length;
+	private static double sumDerivations(Map<String, Double>parameters, String featureKey, String targetClass) {
+		int m = trainingData.length;
 		double returnValue = 0;
 		for (int i=0;i<m;++i) {
-			double xij = data[i].getTrainingFeaturesVector().getFeature(j);
-			returnValue += (h(theta, data[i].getTrainingFeaturesVector())-(data[i].getTrainingDataRecordClass().equals(klass)?1.0:0.0))*xij;
+			double xij = trainingData[i].getTrainingFeaturesVector().getFeature(featureKey);
+			returnValue += (h(parameters, trainingData[i].getTrainingFeaturesVector())-(trainingData[i].getTrainingDataRecordClass().equals(targetClass)?1.0:0.0))*xij;
 		}
 		return returnValue;
 	}
 	
-	public static Map<String, Double> findThetas(Map<String, Double>initialThetas, String klass) throws Exception {
+	public static Map<String, Double> trainParameters(Map<String, Double>initialparameters, String targetClass) throws Exception {
 		for (int i=0;i<numOfIterations;++i) {
-			System.out.println("cost "+i+": "+j(initialThetas, klass));
-			System.out.println("probability: "+h(initialThetas,data[0].getTrainingFeaturesVector()));
-			initialThetas = minJ(initialThetas, klass);
+			System.out.println("cost "+i+": "+j(initialparameters, targetClass));
+			System.out.println("probability: "+h(initialparameters,trainingData[0].getTrainingFeaturesVector()));
+			initialparameters = minJ(initialparameters, targetClass);
 		}
-		return initialThetas;
+		return initialparameters;
 	}
 	
 	public static void main(String[] args) throws Exception {
-		data = new TrainingDataRecord[3];
-		data[0] = new TrainingDataRecord<String>("Zero", "A");
-		data[0].getTrainingFeaturesVector().putFeature("a", 1.0);
-		data[0].getTrainingFeaturesVector().putFeature("b", 1.0);
-		data[0].getTrainingFeaturesVector().putFeature("c", 1.0);
+		trainingData = new TrainingDataRecord[3];
+		trainingData[0] = new TrainingDataRecord<String>("Zero", "A");
+		trainingData[0].getTrainingFeaturesVector().putFeature("a", 1.0);
+		trainingData[0].getTrainingFeaturesVector().putFeature("b", 1.0);
+		trainingData[0].getTrainingFeaturesVector().putFeature("c", 1.0);
 		
-		data[1] = new TrainingDataRecord<String>("Zero", "B");
-		data[1].getTrainingFeaturesVector().putFeature("a", 1.0);
-		data[1].getTrainingFeaturesVector().putFeature("b", 1.0);
-		data[1].getTrainingFeaturesVector().putFeature("d", 1.0);
+		trainingData[1] = new TrainingDataRecord<String>("Zero", "B");
+		trainingData[1].getTrainingFeaturesVector().putFeature("a", 1.0);
+		trainingData[1].getTrainingFeaturesVector().putFeature("b", 1.0);
+		trainingData[1].getTrainingFeaturesVector().putFeature("d", 1.0);
 		
-		data[2] = new TrainingDataRecord<String>("Zero", "C");
-		data[2].getTrainingFeaturesVector().putFeature("b", 1.0);
-		data[2].getTrainingFeaturesVector().putFeature("c", 1.0);
-		data[2].getTrainingFeaturesVector().putFeature("d", 1.0);
+		trainingData[2] = new TrainingDataRecord<String>("Zero", "C");
+		trainingData[2].getTrainingFeaturesVector().putFeature("b", 1.0);
+		trainingData[2].getTrainingFeaturesVector().putFeature("c", 1.0);
+		trainingData[2].getTrainingFeaturesVector().putFeature("d", 1.0);
 		
 		Map<String, Double> thetas = new HashMap<>();
 		thetas.put("Zero", 1.0);
@@ -110,25 +111,23 @@ public class LogisticRegressionModel {
 		thetas.put("d", 1.0);
 		thetas.put("e", 1.0);
 		
-		thetas = findThetas(thetas, "C");
+		thetas = trainParameters(thetas, "C");
 		
 		FeatureVector<String> f = new FeatureVector<>();
 		f.putFeature("a", 1.0);
 		f.putFeature("b", 1.0);
 		f.putFeature("e", 1.0);
 		
-		double p1 = h(thetas,data[0].getTrainingFeaturesVector());
+		double p1 = h(thetas,trainingData[0].getTrainingFeaturesVector());
 		System.out.println("p1="+p1);
 		
-		double p2 = h(thetas,data[1].getTrainingFeaturesVector());
+		double p2 = h(thetas,trainingData[1].getTrainingFeaturesVector());
 		System.out.println("p2="+p2);
 		
-		double p3 = h(thetas,data[2].getTrainingFeaturesVector());
+		double p3 = h(thetas,trainingData[2].getTrainingFeaturesVector());
 		System.out.println("p3="+p3);
 		
 		double pf = h(thetas,f);
 		System.out.println("pf="+pf);
-		
-		
 	}
 }
