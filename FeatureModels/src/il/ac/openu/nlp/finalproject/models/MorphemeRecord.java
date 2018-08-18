54a30eae9d4e9c16d4f4b51eb874610305cb1ce7
Asi Lev <asi.lev@gmail.com>
2018-08-18 11:27:48 +0300

    structured data reader coupling removal from data reading to building vectors

---
+++ FeatureModels/src/il/ac/openu/nlp/finalproject/models/MorphemeRecord.java
@@ -0,0 +1,15 @@
+package il.ac.openu.nlp.finalproject.models;
+
+public class MorphemeRecord {
+	public String originalWord;
+	public String stemmedWord;
+	public String partOfSpeech;
+	public String gender;
+	public String suf_gender;
+	public String num;
+	public String suf_num;
+	public String per;
+	public String suf_per;
+	public String tense;
+	public boolean isPunctuationMark;
+}
--- FeatureModels/src/il/ac/openu/nlp/finalproject/models/StructuredDataReader.java
+++ FeatureModels/src/il/ac/openu/nlp/finalproject/models/StructuredDataReader.java
@@ -6,8 +6,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,11 +17,13 @@ import java.util.regex.Pattern;
 public class StructuredDataReader {
 	// This class handles the YAP output files
 	
+	// Hash from author to a list of sentences. Each sentence is a list of morphemes.
+	private Map<String, List<MorphemeRecord>> userTweets;
 	private boolean bFilterPunctEnabled = true;
 	private boolean bStemmingEnabled = false;
 	private boolean bPunctDetected = false;
 	private File[] listOfFiles = null;
-	private List<TaggedFeatureVector<String>> usersTweets;
+//	private List<TaggedFeatureVector<String>> usersTweets;
 	private String encoding;
 	private Set<String> listOfUniqueWords = new HashSet<>();
 	
@@ -28,20 +32,18 @@ public class StructuredDataReader {
 		File folder = new File(structuredDataPath);
 		listOfFiles = folder.listFiles();
 		this.encoding = encoding;
+		userTweets = new HashMap<>();
 	}
 	
-	private void buildUserWordCountMap(String ZERO_INDEX) throws IOException
+	private void buildUserWordCountMap() throws IOException
 	{
-		usersTweets = new ArrayList<TaggedFeatureVector<String>>();
-
 		for (File file : listOfFiles) {
 		    if (file.isFile()) {
 		        System.out.println("Processing the file: " + file.getName());
 		        String sName = file.getName();
 		        String[] tokens = sName.split("_");
 		        String currUserName = tokens[1];
-		        TaggedFeatureVector<String> newTweet = new TaggedFeatureVector<>(currUserName, ZERO_INDEX);
-		        
+		        List<MorphemeRecord> tweet = new ArrayList<>();
 		        BufferedReader in;
 				in = new BufferedReader(new InputStreamReader(new FileInputStream(file), encoding));
 				Pattern p = Pattern.compile("\\p{Punct}");
@@ -51,32 +53,30 @@ public class StructuredDataReader {
 		        while ((str = in.readLine()) != null) {
 		        	if (str.equals("") == false)
 		        	{
+		        		MorphemeRecord morpheme = new MorphemeRecord();
 			        	tokens = str.split("\t");
 			        	String sWord = "";
-			        	
-			        	if (bStemmingEnabled == false) {
-			        		sWord = tokens[2];
-			        	}
-			        	else {
-			        		sWord = tokens[3];
-			        	}
+			        	morpheme.originalWord = tokens[2];
+			        	morpheme.stemmedWord = tokens[3];
 				        listOfUniqueWords.add(sWord);
 			        	// Check if the token is a punctuation mark
 			        	m = p.matcher(sWord);
 			        	bPunctDetected = m.matches();
+		        		morpheme.isPunctuationMark = bPunctDetected;
 		        		// The word exists in the master word map 
-		        		if (bFilterPunctEnabled == false || (bFilterPunctEnabled == true && bPunctDetected == false))
-		        		{
-		        			newTweet.getFeatureVector().put(sWord, newTweet.getFeatureVector().get(sWord)+1);
-		        		}
+//		        		if (bFilterPunctEnabled == false || (bFilterPunctEnabled == true && bPunctDetected == false))
+//		        		{
+//		        			newTweet.getFeatureVector().put(sWord, newTweet.getFeatureVector().get(sWord)+1);
+//		        		}
+		        		tweet.add(morpheme);
 		        	}
 		        	else
 		        	{
 		        		// An empty line indicates we completed processing a tweet,
 		        		// now we can add it to the user's list of tweets and prepare
 		        		// for the next tweet
-		        		usersTweets.add(newTweet);
-				        newTweet = new TaggedFeatureVector<>(currUserName, ZERO_INDEX);
+		        		userTweets.put(currUserName, tweet);
+				        tweet = new ArrayList<>();
 		        	}
 		        }
 			        
@@ -87,8 +87,16 @@ public class StructuredDataReader {
 	}
 
 	public List<TaggedFeatureVector<String>> buildAuthorBagOfWords(String ZERO_INDEX) throws IOException {
-		buildUserWordCountMap(ZERO_INDEX);
-		return usersTweets;
+		buildUserWordCountMap();
+		List<TaggedFeatureVector<String>> usersTweetsVector = new ArrayList<>();
+		for (Map.Entry<String, List<MorphemeRecord>> tweet : userTweets.entrySet()) {
+			FeatureVector<String> bagOfWords = new FeatureVector<String>(ZERO_INDEX);
+			for (MorphemeRecord morpheme : tweet.getValue()) {
+				bagOfWords.put(morpheme.originalWord, bagOfWords.get(morpheme)+1);
+			}
+			usersTweetsVector.add(new TaggedFeatureVector<>(bagOfWords, tweet.getKey()));
+		}
+		return usersTweetsVector;
 	}
 	
 	public Set<String> getListOfUniqueWords() {
