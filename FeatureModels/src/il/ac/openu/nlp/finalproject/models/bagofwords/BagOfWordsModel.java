3a4cc6f60d34aeaf292a128a00c5b2a36e4f314d
Asi Lev <asi.lev@gmail.com>
2018-08-19 20:04:30 +0300

    Added more BOW methods

--- FeatureModels/src/il/ac/openu/nlp/finalproject/models/bagofwords/BagOfWordsModel.java
+++ FeatureModels/src/il/ac/openu/nlp/finalproject/models/bagofwords/BagOfWordsModel.java
@@ -54,4 +54,49 @@ public class BagOfWordsModel {
 		}
 		return usersTweetsVector;
 	}
+
+	public static List<TaggedFeatureVector<String>> buildAuthorGlobalBagOfWords(Map<String, List<List<MorphemeRecord>>> userTweets, String ZERO_INDEX) throws IOException {
+		List<TaggedFeatureVector<String>> usersTweetsVector = new ArrayList<>();
+		for (Map.Entry<String, List<List<MorphemeRecord>>> user : userTweets.entrySet()) {
+			FeatureVector<String> bagOfWords = new FeatureVector<String>(ZERO_INDEX);
+			for (List<MorphemeRecord> tweet : user.getValue()) {
+				for (MorphemeRecord morpheme : tweet) {
+					bagOfWords.put(morpheme.originalWord, bagOfWords.get(morpheme.originalWord)+1);
+				}
+			}
+			usersTweetsVector.add(new TaggedFeatureVector<>(bagOfWords, user.getKey()));
+		}
+		return usersTweetsVector;
+	}
+
+	public static List<TaggedFeatureVector<String>> buildAuthorGlobalStemmedBagOfWords(Map<String, List<List<MorphemeRecord>>> userTweets, String ZERO_INDEX) throws IOException {
+		List<TaggedFeatureVector<String>> usersTweetsVector = new ArrayList<>();
+		for (Map.Entry<String, List<List<MorphemeRecord>>> user : userTweets.entrySet()) {
+			FeatureVector<String> bagOfWords = new FeatureVector<String>(ZERO_INDEX);
+			for (List<MorphemeRecord> tweet : user.getValue()) {
+				for (MorphemeRecord morpheme : tweet) {
+					bagOfWords.put(morpheme.stemmedWord, bagOfWords.get(morpheme.stemmedWord)+1);
+				}
+			}
+			usersTweetsVector.add(new TaggedFeatureVector<>(bagOfWords, user.getKey()));
+		}
+		return usersTweetsVector;
+	}
+
+	public static List<TaggedFeatureVector<String>> buildAuthorBagOfGlobal2GramsWords(Map<String, List<List<MorphemeRecord>>> userTweets, String ZERO_INDEX) throws IOException {
+		List<TaggedFeatureVector<String>> usersTweetsVector = new ArrayList<>();
+		for (Map.Entry<String, List<List<MorphemeRecord>>> user : userTweets.entrySet()) {
+			FeatureVector<String> bagOfWords = new FeatureVector<String>(ZERO_INDEX);
+			for (List<MorphemeRecord> tweet : user.getValue()) {
+				bagOfWords.put("<SOS> "+tweet.get(0).originalWord, 1.0);
+				for (int i=0; i<tweet.size()-1; ++i) {
+					String _2gram = tweet.get(i).originalWord+" "+tweet.get(i+1).originalWord;
+					bagOfWords.put(_2gram, bagOfWords.get(_2gram)+1);
+				}
+				bagOfWords.put(tweet.get(tweet.size()-1).originalWord+" <EOS>", 1.0);
+			}
+			usersTweetsVector.add(new TaggedFeatureVector<>(bagOfWords, user.getKey()));
+		}
+		return usersTweetsVector;
+	}
 }
